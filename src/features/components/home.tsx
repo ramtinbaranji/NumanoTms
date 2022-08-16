ad47fe71a71cd4a0bdca37ac49b40ce96a2213d7
samira kazemi <samirakazemi0203>
2022-08-16 13:51:23 +0430

    first commit

--- src/features/components/home.tsx
+++ src/features/components/home.tsx
@@ -2,6 +2,17 @@ import * as React from "react";
 import { ReactElement } from "react";
 
 const Home = (): ReactElement => {
-  return <>my project</>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 export default Home;
